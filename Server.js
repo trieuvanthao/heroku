62b29241f97165cc162220094db51722b0109808
trieuvanthao <trieuvanthao95@gmail.com>
2019-06-25 10:09:01 +0700

    fix

--- Server.js
+++ Server.js
@@ -12,11 +12,22 @@
 //
 // // default Heroku PORT
 // app.listen(process.env.PORT || 3000);
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Run the app by serving the static files in the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
