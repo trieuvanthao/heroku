cfe5535047dee0d37410d25dc466088f0fb95f62
trieuvanthao <trieuvanthao95@gmail.com>
2019-06-25 10:03:51 +0700

    fix

--- Server.js
+++ Server.js
@@ -1,14 +1,22 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+// // Serve static files....
+// app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
+//
+// // Send all requests to index.html
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
+// });
+//
+// // default Heroku PORT
+// app.listen(process.env.PORT || 3000);
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
-});
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
