6ac03a14d92c18a3e9c1df55a319f7ec747e2475
trieuvanthao <trieuvanthao95@gmail.com>
2019-06-25 09:58:55 +0700

    fix

--- Server.js
+++ Server.js
@@ -3,13 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-// app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
-app.use(express.static(${__dirname}+ '/front-end/dist/'));
+app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  // res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
-  res.sendFile(`./front-end/dist/index.html`);
+  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
 });
 
 // default Heroku PORT
